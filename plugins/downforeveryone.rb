ffbb247216bff3cf631e075752db72113f6e58b1
Spenser Jones <spenser.e.jones@gmail.com>
2011-09-15 23:32:11 -0600

    Optimized DFEOJM

--- plugins/downforeveryone.rb
+++ plugins/downforeveryone.rb
@@ -1,29 +1,28 @@
 require "mechanize"
-class DownForEveryone
-  include Cinch::Plugin
-
-  def initialize(*args)
-    super
-
-    @agent = Mechanize.new
-    @agent.user_agent_alias = "Linux Mozilla"
-  end
-
-  match /dfeojm (.+)/
-  match /downforeveryone (.+)/
-  
-  def execute(m, url)
-    url = url.gsub(/^https?:\/\//, '')
-    page = @agent.get("http://downforeveryoneorjustme.com/#{url}")
-    if page.title.split(" -> ").first =~ / Up$/
-      m.reply "It's just you. #{url} is up."
-    else
-      m.reply "It's not just you! #{url} looks down from here."
+module SavageBot
+  module Plugins
+    class DownForEveryone
+      include Cinch::Plugin
+      
+      def initialize(*args)
+        super
+        
+        @agent = Mechanize.new
+      end
+      
+      match /dfeojm (.+)/
+      match /downforeveryone (.+)/
+      
+      def execute(m, url)
+        url = url.gsub(/^https?:\/\//, '')
+        page = @agent.get("http://downforeveryoneorjustme.com/#{url}")
+        m.reply(m.user.nick + ': ' + page.search('#container')[0].children[0, 3].inner_text.strip)
+      end
+      
+      listen_to :help, method: :help
+      def help(m)
+        m.reply("!dfeojm domain (!downforeveryone domain) - Check if a website is online or offline")
+      end
     end
-  end
-  
-  listen_to :help, method: :help
-  def help(m)
-    m.reply("!dfeojm domain (!downforeveryone domain) - Check if a website is online or offline")
   end
 end
