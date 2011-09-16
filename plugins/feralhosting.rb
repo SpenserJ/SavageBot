0f74767950f64643dc4a70401e0bc11f2e7ae3df
Spenser Jones <spenser.e.jones@gmail.com>
2011-09-15 18:17:38 -0600

    FeralHosting information is now stored in a table as well

--- plugins/feralhosting.rb
+++ plugins/feralhosting.rb
@@ -1,6 +1,15 @@
-def feral_api(username, request)
-  user = USERS[username.downcase][0]
-  uri = URI.parse("https://www.feralhosting.com/api/json/0.3/#{user}/#{request}")
+class AccountFeralHosting
+  include DataMapper::Resource
+
+  property(:id,         Serial)
+  property(:username,   String, :unique => true)
+  
+  belongs_to :user
+end
+
+def feral_api(m, request)
+  return m.reply("Please connect your FeralHosting account first") unless (user = is_logged_in?(m)) != false && (user = AccountFeralHosting.first(:user => user)).nil? == false
+  uri = URI.parse("https://www.feralhosting.com/api/json/0.3/#{user.username}/#{request}")
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = true
   http.verify_mode = OpenSSL::SSL::VERIFY_NONE
@@ -16,22 +25,40 @@ class FeralHosting
   
   match /(feral )?hdd$/, method: :hdd
   match /(feral )?bandwidth$/, method: :bandwidth
+  match /feral link (.+)/, method: :link
+  match /feral unlink/, method: :unlink
   
   def hdd(m)
     return unless is_configured?(m.user)
-    json = feral_api(m.user.nick, 'server/disk')
+    json = feral_api(m, 'server/disk')
     m.reply "#{m.user.nick}: You have used " + (json['kilobytes'].to_f / 1024 / 1024).round(2).to_s + ' GB of HDD space'
   end
   
   def bandwidth(m)
     return unless is_configured?(m.user)
-    json = feral_api(m.user.nick, 'server/bandwidth')
+    json = feral_api(m, 'server/bandwidth')
     m.reply "#{m.user.nick}: You have uploaded " + (json['upload-external-bytes'].to_f / 1024 / 1024 / 1024).round(2).to_s + ' GB'
+  end
+  
+  def link(m, username)
+    return m.reply("Please sign into Savage first") unless (user = is_logged_in?(m)) != false
+    return m.reply("That FeralHosting account is already linked") if AccountFeralHosting.first(:username => username).nil? == false
+    AccountFeralHosting.create(:username => username, :user => user)
+    m.reply("Credentials have been saved for your FeralHosting account")
+  end
+  
+  def unlink(m)
+    return m.reply("Please sign into Savage first") unless (user = is_logged_in?(m)) != false
+    return m.reply("There is no FeralHosting account linked to #{m.user.authname}") if (link = AccountFeralHosting.first(:user => user)).nil?
+    link.destroy
+    m.reply("That FeralHosting account has been unlinked from #{m.user.authname}")
   end
   
   listen_to :help, method: :help
   def help(m)
     m.reply("!feral hdd (!hdd) - Retrieve your HDD usage from FeralHosting")
     m.reply("!feral bandwidth (!bandwidth) - Retrieve your bandwidth usage from FeralHosting")
+    m.reply("!feral link username - Connect your Savage account to a FeralHosting account")
+    m.reply("!feral unlink - Break the connection between your Savage account and FeralHosting account")
   end
 end
